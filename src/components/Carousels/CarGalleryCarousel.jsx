aad4d9cb8f79fd58b234829c2c91f8364ca9de88
Petras Butkus <p.butkus360@gmail.com>
2023-06-29 13:40:15 +0300

    car gallery carousel rewrite

--- src/components/Carousels/CarGalleryCarousel.jsx
+++ src/components/Carousels/CarGalleryCarousel.jsx
@@ -1,44 +1,132 @@
-import { useState } from "preact/hooks";
+import { Component, createRef } from "preact";
 
-export default function CarGalleryCarousel({ photos }) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [prevIndex, setPrevIndex] = useState(photos.length - 1);
-  const [nextIndex, setNextIndex] = useState(1);
+export default class CarGalleryCarousel extends Component {
+  constructor(props) {
+    super(props);
+    this.photos = this.props.photos;
+    this.state = {
+      animationRunning: false,
+      currentIndex: 0,
+      prevIndex: this.props.photos.length - 1,
+      nextIndex: 1,
+      currentClass: "",
+      prevClass: "opacity-0",
+      nextClass: "opacity-0",
+      imgWidth: 600,
+    };
+    this.carouselContainerRef = createRef();
+  }
 
-  const handlePrev = () => {
-    if (prevIndex == 0) {
-      setPrevIndex(photos.length - 1);
-      setCurrentIndex(0);
-      setNextIndex(1);
-    } else {
-      setNextIndex(currentIndex);
-      setCurrentIndex(prevIndex);
-      setPrevIndex(prevIndex - 1);
+  componentDidMount() {
+    console.log(this.carouselContainerRef.current.clientWidth);
+    this.setState({
+      imgWidth: this.carouselContainerRef.current.clientWidth + 10,
+    });
+  }
+
+  handlePrev = () => {
+    if (!this.state.animationRunning) {
+      this.setState({
+        animationRunning: true,
+        currentClass: "translate-x-full transition-all duration-500 opacity-0",
+        prevClass: "translate-x-full transition-all duration-500 opacity-1",
+        nextClass: "translate-x-full transition-all duration-500",
+      });
+      setTimeout(() => {
+        if (this.state.prevIndex == 0) {
+          this.setState({
+            prevIndex: this.photos.length - 1,
+            currentIndex: 0,
+            nextIndex: 1,
+          });
+        } else {
+          this.setState({
+            nextIndex: this.state.currentIndex,
+            currentIndex: this.state.prevIndex,
+            prevIndex: this.state.prevIndex - 1,
+          });
+        }
+        this.setState({
+          animationRunning: false,
+          currentClass: "",
+          prevClass: "opacity-0",
+          nextClass: "opacity-0",
+        });
+      }, 500);
     }
-
-    console.log(prevIndex);
   };
 
-  const handleNext = () => {
-    if (nextIndex == photos.length - 1) {
-      setCurrentIndex(photos.length - 1);
-      setPrevIndex(photos.length - 2);
-      setNextIndex(0);
-    } else {
-      setPrevIndex(currentIndex);
-      setCurrentIndex(nextIndex);
-      setNextIndex(nextIndex + 1);
+  handleNext = () => {
+    if (!this.state.animationRunning) {
+      this.setState({
+        animationRunning: true,
+        currentClass: "-translate-x-full transition-all duration-500 opacity-0",
+        prevClass: "-translate-x-full transition-all duration-500 opacity-1",
+        nextClass: "-translate-x-full transition-all duration-500",
+      });
+      setTimeout(() => {
+        if (this.state.nextIndex == this.photos.length - 1) {
+          this.setState({
+            currentIndex: this.photos.length - 1,
+            prevIndex: this.photos.length - 2,
+            nextIndex: 0,
+          });
+        } else {
+          this.setState({
+            prevIndex: this.state.currentIndex,
+            currentIndex: this.state.nextIndex,
+            nextIndex: this.state.nextIndex + 1,
+          });
+        }
+        this.setState({
+          animationRunning: false,
+          currentClass: "",
+          prevClass: "opacity-0",
+          nextClass: "opacity-0",
+        });
+      }, 500);
     }
   };
 
-  return (
-    <>
-      <div className="flex justify-center items-center w-full">
-        <div>
-          <div className={"flex flex-row overflow-auto"}>
-            <div className={"w-full h-auto relative"}>
+  render() {
+    return (
+      <>
+        <div
+          id={"carousel-container"}
+          ref={this.carouselContainerRef}
+          className="flex justify-center items-center w-full"
+        >
+          <div className={"flex flex-row overflow-hidden w-full"}>
+            <div className={"flex flex-row h-auto relative justify-center"}>
+              <img
+                className={
+                  "pointer-events-none object-cover aspect-video h-96 " +
+                  this.state.prevClass
+                }
+                src={this.props.photos[this.state.prevIndex]}
+                alt="null"
+                width="1500px"
+              />
+              <img
+                className={
+                  "pointer-events-none object-cover aspect-video h-96 " +
+                  this.state.currentClass
+                }
+                src={this.props.photos[this.state.currentIndex]}
+                alt="null"
+                width="1500px"
+              />
+              <img
+                className={
+                  "pointer-events-none object-cover aspect-video h-96 " +
+                  this.state.nextClass
+                }
+                src={this.props.photos[this.state.nextIndex]}
+                alt="null"
+                width="1500px"
+              />
               <button
-                onClick={handlePrev}
+                onClick={this.handlePrev}
                 className={"absolute top-0 bottom-0 my-auto mx-0 left-0"}
               >
                 <svg
@@ -49,14 +137,9 @@ export default function CarGalleryCarousel({ photos }) {
                   <path d="M14.41 18.16L8.75 12.5l5.66-5.66l.7.71l-4.95 4.95l4.95 4.95l-.7.71Z"></path>
                 </svg>
               </button>
-              <img
-                className="pointer-events-none object-cover aspect-video h-96 w-full"
-                src={photos[currentIndex]}
-                alt="null"
-              />
               <button
-                onClick={handleNext}
-                className={"absolute top-0 bottom-0 my-auto mx-0 right-0"}
+                onClick={this.handleNext}
+                className={"absolute z-50 top-0 bottom-0 my-auto mx-0 right-0"}
               >
                 <svg
                   class="w-10 h-10 bg-white/50 hover:bg-white/70 transition"
@@ -69,7 +152,7 @@ export default function CarGalleryCarousel({ photos }) {
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
